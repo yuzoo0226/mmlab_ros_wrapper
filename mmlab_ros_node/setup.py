095fc4882425ca67fce25e73f2912e833d9bcc33
Yuga Yano <74362091+yuzoo0226@users.noreply.github.com>
2024-11-12 11:42:34 +0900

    configの追加

--- mmlab_ros_node/setup.py
+++ mmlab_ros_node/setup.py
@@ -12,17 +12,24 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
 
-        # モデルの追加はここに書く
+        # モデルとconfigの追加はここに書く
         (os.path.join('share', package_name, 'config/mmaction/detection/video_mae/'), glob.glob('config/mmaction/detection/video_mae/*.py')),
         (os.path.join('share', package_name, 'config/mmaction/label_map/'), glob.glob('config/mmaction/label_map/*.txt')),
         (os.path.join('share', package_name, 'config/mmaction/detection/recognition/tsn/ort/'), glob.glob('config/mmaction/recognition/tsn/ort/*.json')),
         (os.path.join('share', package_name, 'config/mmaction/detection/recognition/videomae/ort'), glob.glob('config/mmaction/recognition//videomae/ort/*.json')),
         (os.path.join('share', package_name, 'config/mmdetection/faster_rcnn/'), glob.glob('config/mmdetection/faster_rcnn/*.py')),
         (os.path.join('share', package_name, 'config/mmpose/seresnet/'), glob.glob('config/mmpose/seresnet/*.py')),
+
+        # Tracking
+        (os.path.join('share', package_name, 'config/mmtracking/reid/'), glob.glob('config/mmtracking/reid/*.py')),
+        (os.path.join('share', package_name, 'config/mmtracking/_base_/'), glob.glob('config/mmtracking/_base_/*.py')),
+        (os.path.join('share', package_name, 'config/mmtracking/_base_/datasets/'), glob.glob('config/mmtracking/_base_/datasets/*.py')),
+
+        # models
         (os.path.join('share', package_name, 'config/ckpt/'), glob.glob('config/ckpt/*.pth')),
         (os.path.join('share', package_name, 'config/ckpt/'), glob.glob('config/ckpt/*.onnx')),
-
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='toyota',
